7e004be16f2798bfaaacb0b1a6424dfeb04acee0
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-08-09 18:04:33 +0200

    Additional tmux fixes

--- bin/tmux_domain.sh
+++ bin/tmux_domain.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 tmux_domain=''
+MAX_LENGTH=32
 
-domain=$(hostname -d 2>/dev/null)
-colour=colour$((16#$( $domain | md5sum | cut -b-2)))
-tmux_domain="#[fg=colour16,bg=${colour},bold] ${domain}"
 
+d=$(hostname -d 2>/dev/null)
+if ! [[ -z $d ]]; then
+    if [ ${#d} -lt $MAX_LENGTH ]; then
+        domain=$d;
+    else
+        # sublength=$(($MAX_LENGTH/2-2))
+        # domain=${d:0:${sublength}}...${d:$NF-${sublength}:${sublength}}
+        domain=${d:$NF-32:32}
+    fi
+    colour=colour$((16#$(echo $d | md5sum | cut -b-2)))
+    tmux_domain="#[fg=colour15,bg=${colour},bold] ${domain}";
+fi
 echo "$tmux_domain "
--- bin/tmux_puppet.sh
+++ bin/tmux_puppet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tmux_puppet=''
 
-if sudo /etc/init.d/puppet status | grep -q 'puppet cronjob is disabled'; then
-    tmux_puppet='#[fg=colour0,bg=colour15,bold] puppet stopped '
-elif sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf; then
-    tmux_puppet='#[fg=colour15,bg=colour9,bold] puppet disabled '
+if sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf; then
+    tmux_puppet='#[fg=colour15,bg=colour9,bold] ✘ puppet disabled '
+elif sudo /etc/init.d/puppet status | grep -q 'puppet cronjob is disabled'; then
+    tmux_puppet='#[fg=colour0,bg=colour15,bold] ✘ puppet stopped '
 fi
 
 echo "$tmux_puppet"
