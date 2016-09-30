d9445a1f851127c663361d4149299b329ae1df9e
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-09-30 14:29:39 +0200

    Tmux tuning
     - Make tmux_puppet use init.d daemon (legacy support)
     - Set update interval to 5min
     - Introduce tmux_users, which will inform if someone else is logged in

--- .tmux.conf.d/tmux_puppet.sh
+++ .tmux.conf.d/tmux_puppet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf && echo '#[fg=colour15,bg=colour9,bold] ✘ puppet disabled ' || sudo service puppet status 2>&1 >/dev/null || echo '#[fg=colour0,bg=colour15,bold] ✘ puppet stopped '
+sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf && echo '#[fg=colour15,bg=colour9,bold] ✘ puppet disabled ' || sudo /etc/init.d/puppet status 2>&1 >/dev/null || echo '#[fg=colour0,bg=colour15,bold] ✘ puppet stopped '
