8a8cfa4468c2588932091c032dd556d30f06155d
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-10-11 17:08:22 +0200

    Update liquidprompt

--- .config/liquidprompt/globalundo.ps1
+++ .config/liquidprompt/globalundo.ps1
@@ -17,8 +17,8 @@
 # LP_ERR last error code
 # LP_MARK prompt mark
 # LP_TIME current time
+# LP_TTYN number of current terminal (useful in title for quick switching)
 # LP_RUNTIME runtime of last command
-# LP_MARK_PREFIX user-defined prompt mark prefix (helpful if you want 2-line prompts)
 # LP_PS1_PREFIX user-defined general-purpose prefix (default set a generic prompt as the window title)
 # LP_PS1_POSTFIX user-defined general-purpose postfix
 # LP_BRACKET_OPEN open bracket
@@ -34,14 +34,13 @@ LP_PS1="${LP_PS1}${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
 
 LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
 
-# # Add VCS infos
-# # If root, the info has not been collected unless LP_ENABLE_VCS_ROOT
-# # is set.
+# Add VCS infos
+# If root, the info has not been collected unless LP_ENABLE_VCS_ROOT
+# is set.
 LP_PS1="${LP_PS1}${LP_VCS}"
 
 # # add return code and prompt mark
-LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}\n${LP_MARK}${LP_PS1_POSTFIX}"
-# LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
+LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}${LP_PS1_POSTFIX}"
 
 # "invisible" parts
 # Get the current prompt on the fly and make it a title
