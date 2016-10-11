6f6e8949b4e371473acd0450e7f3f09651ba78ba
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-10-11 15:20:33 +0200

    Add contrast FG color

--- .tmux.conf.d/tmux_users.sh
+++ .tmux.conf.d/tmux_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-USERS=$(who -q | head -n 1 | sed "s/$USER//g" | tr -d '\n')
-[ -z "$USERS" ] || echo " #[fg=colour16,bg=colour11,bold]${USERS} "
+USERS=$(who -q | head -n 1 | sed -E "s/\s+?$USER\s+?//g" | tr -d '\n')
+[ -z "$USERS" ] || echo "#[fg=colour16,bg=colour11,bold] ${USERS} "
