7d0fe5b5a7bafe8606a227db95d0b304b18488a2
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-10-19 10:55:33 +0200

    Make tmux_puppet systemd-compatible
    Also, add SUDO_ASKPASS

--- .tmux.conf.d/tmux_puppet.sh
+++ .tmux.conf.d/tmux_puppet.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf && echo '#[fg=colour15,bg=colour9,bold] ✘ puppet disabled ' || sudo /etc/init.d/puppet status 2>&1 >/dev/null || echo '#[fg=colour0,bg=colour15,bold] ✘ puppet stopped '
+export SUDO_ASKPASS=/bin/false
+
+if sudo grep -qE 'server.+ask' /etc/puppet/puppet.conf;
+then
+    echo '#[fg=colour15,bg=colour9,bold] ✘ puppet disabled ';
+else
+    [ -f /etc/init.d/puppet ] && daemon='/etc/init.d/puppet' || daemon='service puppet'
+    sudo $daemon status 2>&1 >/dev/null || echo '#[fg=colour0,bg=colour15,bold] ✘ puppet stopped '
+fi
