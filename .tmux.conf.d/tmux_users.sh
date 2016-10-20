8cf7d9e092898549c997dc94779749cbde6ac8a9
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2016-10-20 14:14:26 +0200

    improve tmux_users.sh
     - remove trailing spaces
     - remove duplicates

--- .tmux.conf.d/tmux_users.sh
+++ .tmux.conf.d/tmux_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-USERS=$(who -q | head -n 1 | sed -E "s/\s+?$USER\s+?//g" | tr -d '\n')
-[ -z "$USERS" ] || echo "#[fg=colour16,bg=colour11,bold] ${USERS} "
+USERS=$(who | awk '{print $1}' | grep -v oleg.mikhaylov | sort | uniq | tr '\n' ' ')
+[ -z "$USERS" ] || echo "#[fg=colour16,bg=colour11,bold] ${USERS}"
