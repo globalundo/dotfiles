38979795272275237ae1e7102956280705e8f438
Oleg Mikhaylov <oleg.mikhaylov@kaufda.de>
2017-06-06 17:39:33 +0200

    Import dotfiles

--- .tmux.conf.d/tmux_users.sh
+++ .tmux.conf.d/tmux_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-USERS=$(who | awk '{print $1}' | grep -v $USER | sort | uniq | tr '\n' ' ')
+USERS=$(who | awk '{print $1}' | grep -v "$USER" | sort | uniq | tr '\n' ' ')
 [ -z "$USERS" ] || echo "#[fg=colour16,bg=colour11,bold] ${USERS}"
