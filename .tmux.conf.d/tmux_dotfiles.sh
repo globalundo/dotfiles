3bea5ddddca099521df19d33e7b99b3d7d3fec23
Oleg Mikhaylov <oleg.mikhaylov@flixbus.com>
2023-06-06 13:53:47 +0200

    Upgrade tmux.conf to version 3

--- .tmux.conf.d/tmux_dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ENV_ETAG=${LC_PAPER:='empty env tag'}
-FILES_ETAG=$(cat ~/.dotfiles-etag 2>/dev/null || echo "empty files tag")
-if [ "$ENV_ETAG" != "$FILES_ETAG" ]; then
-    echo '#[fg=colour11,bg=colour6,bold] ▲ ';
-else
-    echo ''
-fi
--- .tmux.conf.d/tmux_users.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-USERS=$(who | awk '{print $1}' | grep -v "$USER" | sort | uniq | tr '\n' ' ')
-[ -z "$USERS" ] || echo "#[fg=colour16,bg=colour11,bold] ${USERS}"
